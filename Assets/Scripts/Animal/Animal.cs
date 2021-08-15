a295700c0e96b3df573dbfb46cc928c85d89c6e1
Anastasiia Mitinova <nastyawnastya@gmail.com>
2021-08-15 23:18:20 +0300

    Create game scene with empty panels

---
+++ Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField]
+    private SkeletonAnimation skeletonAnimation;
+    void Awake()
+    {
+        skeletonAnimation.loop = true;
+        skeletonAnimation.AnimationName = "Walk";
+    }
+}
---
+++ Assets/Scripts/PanelSafeArea.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PanelSafeArea : MonoBehaviour
+{
+    public Canvas canvas;
+    RectTransform panelSafeArea;
+    Rect currentSafeArea;
+    ScreenOrientation currentOrientation;
+    public RectTransform leftPanel, rightPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelSafeArea = GetComponent<RectTransform>();
+        currentSafeArea = Screen.safeArea;
+        applySafeArea();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentOrientation != Screen.orientation || currentSafeArea != Screen.safeArea)
+        {
+            applySafeArea();
+        }
+    }
+
+    void applySafeArea()
+    {
+        if (panelSafeArea == null)
+        {
+            return;
+        }
+
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= canvas.pixelRect.width;
+        anchorMin.y /= canvas.pixelRect.height;
+
+        anchorMax.x /= canvas.pixelRect.width;
+        anchorMax.y /= canvas.pixelRect.height;
+
+        panelSafeArea.anchorMin = anchorMin;
+        panelSafeArea.anchorMax = anchorMax;
+
+        leftPanel.sizeDelta = new Vector2((safeArea.size.x / 2f), leftPanel.sizeDelta.y);
+        rightPanel.sizeDelta = new Vector2((safeArea.size.x / 2f), leftPanel.sizeDelta.y);
+
+        currentOrientation = Screen.orientation;
+        currentSafeArea = Screen.safeArea;
+    }
+}
